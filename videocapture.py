b1865e10887ae2fc43432d76cbd9d8ec237e5b9d
kimbongju02 <qhdwn25@gmail.com>
2024-06-02 11:57:01 +0900

    capture image save

--- videocapture.py
+++ videocapture.py
@@ -1,10 +1,9 @@
 import cv2
 import time
 from detect import detect
-import time
 
 def gstreamer_pipeline(
-    sensor_id=0,
+    sensor_mode=0,
     capture_width=480,
     capture_height=320,
     display_width=480,
@@ -13,14 +12,14 @@ def gstreamer_pipeline(
     flip_method=0,
 ):
     return (
-        "nvarguscamerasrc sensor-id=%d ! "
-        "video/x-raw(memory:NVMM), width=(int)%d, height=(int)%d, framerate=(fraction)%d/1 ! "
+        "gst-launch-1.0 nvarguscamerasrc sensor_mode=%d ! "
+        "\'video/x-raw(memory:NVMM),width=%d,height=%d,framerate=%d/1\' ! "
         "nvvidconv flip-method=%d ! "
-        "video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! "
+        "\'video/x-raw,width=%d,height=%d\' ! "
         "videoconvert ! "
-        "video/x-raw, format=(string)BGR ! appsink"
+        "video/x-raw, format=NV12 ! appsink"
         % (
-            sensor_id,
+            sensor_mode,
             capture_width,
             capture_height,
             framerate,
@@ -29,8 +28,11 @@ def gstreamer_pipeline(
             display_height,
         )
     )
+print("------------print gstreamer_pipeline(flip_method=0)------------ \n", gstreamer_pipeline(flip_method=0))
 
 capture = cv2.VideoCapture(gstreamer_pipeline(flip_method=0), cv2.CAP_GSTREAMER)
+capture.set(cv2.CAP_PROP_FRAME_WIDTH, 480) # Length
+capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 320) # Width
 
 try:
     while True:
@@ -39,8 +41,8 @@ try:
         if not retval:
                 print('can\'t read frame')
         
-        cv2.imwrite(filename, frame)
-        detect()
+        #cv2.imwrite(filename, frame)
+        #detect()
         
         time.sleep(5)
 finally:
