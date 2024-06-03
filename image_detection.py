45c86030a2717c8bee23595a8e5161e21dc6ce0d
kimbongju02 <qhdwn25@gmail.com>
2024-06-03 13:27:31 +0900

    check time about detection

---
+++ image_detection.py
@@ -0,0 +1,50 @@
+import cv2
+import time
+import detect
+
+def gstreamer_pipeline(
+    sensor_id=0,
+    capture_width=480,
+    capture_height=320,
+    display_width=480,
+    display_height=320,
+    framerate=30,
+    flip_method=0,
+):
+    return (
+        "nvarguscamerasrc sensor-id=%d ! "
+        "video/x-raw(memory:NVMM), width=(int)%d, height=(int)%d, framerate=(fraction)%d/1 ! "
+        "nvvidconv flip-method=%d ! "
+        "video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! "
+        "videoconvert ! "
+        "video/x-raw, format=(string)BGR ! appsink"
+        % (
+            sensor_id,
+            capture_width,
+            capture_height,
+            framerate,
+            flip_method,
+            display_width,
+            display_height,
+        )
+    )
+print("------------print gstreamer_pipeline(flip_method=0)------------ \n", gstreamer_pipeline(flip_method=0))
+
+capture = cv2.VideoCapture(gstreamer_pipeline(flip_method=0), cv2.CAP_GSTREAMER)
+
+try:
+    while True:
+        retval, frame = capture.read()
+
+        if not retval:
+            print('can\'t read frame')
+        cv2.imwrite("./data/images/test.jpg", frame)
+        print("----------------success save image----------------")
+        start_time = time.time()
+        detect.detect()
+        end_time = time.time()
+        elapsed_time = end_time - start_time
+        print(f"실행 시간: {elapsed_time}초")
+        quit()
+finally:
+    capture.release()
